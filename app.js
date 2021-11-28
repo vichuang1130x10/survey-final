bee07ebc0fe97c3c2f6a02ee61ed576de21aff97
vic huang <vic.huang1130x10@gmail.com>
2021-11-28 15:09:17 +0800

    add server

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+
+const path = require("path");
+
+const port = process.env.PORT || 5050;
+
+app.use(express.static("build"));
+app.get("*", (req, res) => {
+  req.sendFile(path.resolve(__dirname, "build", "index.html"));
+});
+
+app.listen(port, (err) => {
+  if (err) return console.log(err);
+  console.log("App running on port: ", port);
+});
