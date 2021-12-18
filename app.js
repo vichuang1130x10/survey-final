f7461d822f89ecb26cdf905e00484aa601225cd6
vic huang <vic.huang1130x10@gmail.com>
2021-12-18 14:23:03 +0800

    fixed server get url failed issue

--- app.js
+++ app.js
@@ -6,8 +6,16 @@ const path = require("path");
 const port = process.env.PORT || 5050;
 
 app.use(express.static("build"));
-app.get("/", (req, res) => {
-  req.sendFile(path.resolve(__dirname, "build", "index.html"));
+// app.get("/", (req, res) => {
+//   req.sendFile(path.resolve(__dirname, "build", "index.html"));
+// });
+
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname, "build/index.html"), function (err) {
+    if (err) {
+      res.status(500).send(err);
+    }
+  });
 });
 
 app.listen(port, (err) => {
